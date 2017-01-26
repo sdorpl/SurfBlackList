dcf548601c54b2724d56ca115ad1628bab78c163
sdorpl <macszymk@outlook.com>
2017-01-26 16:24:16 +0100

    Poprawki
    Poprawki

--- functions/functions.php
+++ functions/functions.php
@@ -5,9 +5,9 @@ function CheckBlackList($url, $domain, $sid)
 {
 	$urlParsed = parse_url($url);
 
-	if(empty($urlParsed["query"])) 
+	if(empty($urlParsed['query'])) 
 	{
-		if(empty($urlParsed["scheme"])) 
+		if(empty($urlParsed['scheme'])) 
 		{
 			$u = $urlParsed['path'];
 		} 
@@ -18,7 +18,7 @@ function CheckBlackList($url, $domain, $sid)
 	} 
 	else 
 	{
-		if(empty($urlParsed["scheme"])) 
+		if(empty($urlParsed['scheme'])) 
 		{
 			$u = $urlParsed['path'].'/?'.$urlParsed['query'];
 		} 
@@ -33,14 +33,14 @@ function CheckBlackList($url, $domain, $sid)
 	//Przetwarzam wynik sprawdzania listy
 	if($blacklist[0]['result'] == 'TRUE')
 	{
-		//Komunikat z info, że strona ma bana. $blacklist[0]['reason'] zawiera powód bana.
+		//Strona ma bana. $blacklist[0]['reason'] zawiera powód bana.
 		$result = true;
 		$reason = $blacklist[0]['reason'];
 		return array($result, $reason);
 	} 
 	elseif($blacklist[0]['result'] == 'FALSE')
 	{
-		//Skrypt dodający stronę do bazy. Strana nie ma bana i może być dodana do surfa i/lub ptp.
+		//Strana nie ma bana i może być dodana do surfa i/lub ptp.
 		$result = false;
 		$reason = '';
 		return array($result, $reason);
@@ -51,9 +51,9 @@ function SendToBlackList($url, $reason = "nie podano", $domain, $sid)
 {
 	$urlParsed = parse_url($url);
 
-	if(empty($urlParsed["query"])) 
+	if(empty($urlParsed['query'])) 
 	{
-		if(empty($urlParsed["scheme"])) 
+		if(empty($urlParsed['scheme'])) 
 		{
 			$u = $urlParsed['path'];
 		} 
@@ -64,7 +64,7 @@ function SendToBlackList($url, $reason = "nie podano", $domain, $sid)
 	} 
 	else 
 	{
-		if(empty($urlParsed["scheme"])) 
+		if(empty($urlParsed['scheme'])) 
 		{
 			$u = $urlParsed['path'].'/?'.$urlParsed['query'];
 		} 
@@ -92,99 +92,4 @@ function SendToBlackList($url, $reason = "nie podano", $domain, $sid)
 		return array($result, $reason);
 	}
 }
-=======
-<?php
-
-function CheckBlackList($url, $domain, $sid)
-{
-	$urlParsed = parse_url($url);
-
-	if(empty($urlParsed["query"])) 
-	{
-		if(empty($urlParsed["scheme"])) 
-		{
-			$u = $urlParsed['path'];
-		} 
-		else 
-		{
-			$u = $urlParsed['host'].$urlParsed['path'];
-		}
-	} 
-	else 
-	{
-		if(empty($urlParsed["scheme"])) 
-		{
-			$u = $urlParsed['path'].'/?'.$urlParsed['query'];
-		} 
-		else 
-		{
-			$u = $urlParsed['host'].$urlParsed['path'].'/?'.$urlParsed['query'];
-		}
-	}
-	
-	$blacklist = json_decode(file_get_contents('https://sdor.pl/json/json.php?r=check&domain='.$domain.'&sid='.$sid.'&url='.$u), true);
-
-	//Przetwarzam wynik sprawdzania listy
-	if($blacklist[0]['result'] == 'TRUE')
-	{
-		//Komunikat z info, że strona ma bana. $blacklist[0]['reason'] zawiera powód bana.
-		$result = true;
-		$reason = $blacklist[0]['reason'];
-		return array($result, $reason);
-	} 
-	elseif($blacklist[0]['result'] == 'FALSE')
-	{
-		//Skrypt dodający stronę do bazy. Strana nie ma bana i może być dodana do surfa i/lub ptp.
-		$result = false;
-		$reason = '';
-		return array($result, $reason);
-	}
-}
-
-function SendToBlackList($url, $reason = "nie podano", $domain, $sid)
-{
-	$urlParsed = parse_url($url);
-
-	if(empty($urlParsed["query"])) 
-	{
-		if(empty($urlParsed["scheme"])) 
-		{
-			$u = $urlParsed['path'];
-		} 
-		else 
-		{
-			$u = $urlParsed['host'].$urlParsed['path'];
-		}
-	} 
-	else 
-	{
-		if(empty($urlParsed["scheme"])) 
-		{
-			$u = $urlParsed['path'].'/?'.$urlParsed['query'];
-		} 
-		else 
-		{
-			$u = $urlParsed['host'].$urlParsed['path'].'/?'.$urlParsed['query'];
-		}
-	}
-	
-	$blacklist = json_encode(file_get_contents('https://sdor.pl/json/json.php?r=send&domain='.$domain.'&sid='.$sid.'&url='.$u.'&reason='.$reason), true);
-
-	//Przetwarzam wynik sprawdzania listy
-	if($blacklist[0]['result'] == 'TRUE')
-	{
-		//Komunikat z info, że strona ma bana. $blacklist[0]['reason'] zawiera powód bana.
-		$result = true;
-		$reason = $blacklist[0]['reason'];
-		return array($result, $reason);
-	} 
-	elseif($blacklist[0]['result'] == 'FALSE')
-	{
-		//Skrypt dodający stronę do bazy. Strana nie ma bana i może być dodana do surfa i/lub ptp.
-		$result = false;
-		$reason = '';
-		return array($result, $reason);
-	}
-}
->>>>>>> origin/master
 ?>
